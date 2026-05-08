d838a926a4c263011ca6b41ae81731b0f5a9a584
Dre <randrefearon@gmail.com>
2026-05-07 23:37:52 -0500

    Added CORS to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -12,5 +14,7 @@ migrate = Migrate(app, db)
 
 from app.views import api
 app.register_blueprint(api, url_prefix='/api/v1')
+jwt = JWTManager(app)
+CORS(app)
 
 from app import views, models
